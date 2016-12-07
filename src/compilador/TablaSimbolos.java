6aec157927f573e70cc29b60ffc040fa56771623
Beatriz Cortés <beatriz.cortes@edu.uah.es>
2016-12-07 17:42:35 +0100

    Inserted dynamic semantic checks
    Specifically, initialization check when reading
    Only tree display left

--- src/compilador/TablaSimbolos.java
+++ src/compilador/TablaSimbolos.java
@@ -4,16 +4,19 @@ import java.util.HashMap;
 
 public class TablaSimbolos{
 	
-	HashMap<String,String> tabla;
+	HashMap<String,String> tablaTipos;
 	//Claves son los identificadores. Valores son los tipos
 	//Tipos posibles: Integer, Boolean, Undefined, Pseudo
+	HashMap<String,Boolean> tablaInicializado;
+	//Clave los identificadores. Valores es si esta inicializado o no (se hara al asignar)
 	
 	/**
 	 * Constructor de TablaSimbolos. 
 	 * Inicializa el hashmap.
 	 */
 	public TablaSimbolos(){
-		tabla = new HashMap<String,String>();
+		tablaTipos = new HashMap<String,String>();
+		tablaInicializado = new HashMap<String,Boolean>();
 	}
 	
 	/**
@@ -23,7 +26,7 @@ public class TablaSimbolos{
 	 * @return tipo de simbolo
 	 */
 	public String getSimbolo(String s){
-		if(tabla.containsKey(s))return tabla.get(s);
+		if(tablaTipos.containsKey(s))return tablaTipos.get(s);
 		else return "Undefined";
 	}
 	
@@ -33,6 +36,25 @@ public class TablaSimbolos{
 	 * @param t tipo del símbolo
 	 */
 	public void insertarSimbolo(String s, String t){
-		tabla.put(s, t);
+		tablaTipos.put(s, t);
+		tablaInicializado.put(s, false);
+	}
+	/**
+	 * Devuelve el estado de inicializacion de un simbolo de la tabla
+	 * @param s simbolo a buscar
+	 * @return inicializado o no
+	 */
+	public boolean estaInicializado(String s){
+		return tablaInicializado.get(s);
+	}
+	/**
+	 * Coloca el símbolo de la tabla como inicializado. Si ya lo es, no hace nada
+	 * @param s a actualizar
+	 */
+	public void inicializarSimbolo(String s){
+		if(!tablaInicializado.get(s)){
+			tablaInicializado.remove(s);
+			tablaInicializado.put(s, true);
+		}
 	}
 }
